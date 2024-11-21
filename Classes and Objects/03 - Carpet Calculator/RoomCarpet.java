d6190e3fff0abf680a66e54570d585111fb53d53
mlun1201 <mlun1149@gmail.com>
2024-11-21 03:04:51 -0600

    Update RoomCarpet.java

--- Classes and Objects/03 - Carpet Calculator/RoomCarpet.java
+++ Classes and Objects/03 - Carpet Calculator/RoomCarpet.java
@@ -1,17 +1,21 @@
 public class RoomCarpet
 {
+    //fields
     private RoomDimension size;
     private double carpetCost;
 
+    //constructor, take RoomDimension object
     public RoomCarpet(RoomDimension dim, double cost){
         size = dim;
         carpetCost = cost;
     }
 
+    //call method from RoomDimension to calculate and return total cost
     public double getTotalCost(){
         return size.getArea()*carpetCost;
     }
 
+    //display cost
     public String toString(){
         return("The carpet cost per square foot is $" + carpetCost + " so the total cost of the carpet will be $" + getTotalCost());
     }
